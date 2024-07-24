5e3419d23df55e6b8facd33c4a9660bb5a55df5a
Reshma.CA <reshmacaci@gmail.com>
2024-07-24 18:10:47 +0530

    Second

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Exercse no 1
+// Exercise no 1
 
 // 1, create a variable named "Price" and set it to a specific price value.
 // 2, create a variable named "Product" and set it to a specific product name.
@@ -12,3 +12,22 @@ var Tax = 500;
 
 console.log("Product name:",Product);
 console.log("Total amount:",Price+Tax);
+
+// Exercise no 2
+
+// Declare a variable named "FruitName".
+// Declare a variable named "Count".
+// Declare a variable named "Price" and set it to the price of the fruit.
+
+// Declare a variable named "total" and calculate it by multiplying the "Count" by the "Price".
+
+//Print the name of the fruit.
+//Print the total price of the fruit.
+
+var FruitName = "Orange";
+var Count = 5;
+var Price = 10;
+
+var total = Count*Price;
+console.log("Name of the fruit:",FruitName)
+console.log("Total Price:",total)
