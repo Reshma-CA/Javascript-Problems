8e55fe84802629ca9c3f01656b51203079c9785f
Reshma.CA <reshmacaci@gmail.com>
2024-07-24 17:39:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Exercse no 1
+
+// 1, create a variable named "Price" and set it to a specific price value.
+// 2, create a variable named "Product" and set it to a specific product name.
+// 3,create a variable named "Tax" and set it to a specific tax value.
+//4, print the name of the product.
+//5, calculate the total price amount,including tax(total=Price+Tax),and then print it.
+
+var Price = 78000;
+var Product= "Smasung S24"
+var Tax = 500;
+
+console.log("Product name:",Product);
+console.log("Total amount:",Price+Tax);
